c3019d8c4e8b5d8160860eadf64b7bd271750117
lesahley <lesashley56@gmail.com>
2017-07-17 10:33:49 -0500

    Agregando animacion

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
- $('.menu').click(function(e){
- 	e.preventDefault();
- 	$('html, body').stop().animate({scrollTop: $($('.menu').attr('href')).offset().top}, 1000);
- });
-
-(_ =>{
-  // $('#description').scroll(function () {
-    // var n = $('#description').scrollTop();
-    // console.log(n);
-    // console.log("hola");
-  // });
-console.log($('#description').scrollTop());
+$('.menu').click(function(e){
+  e.preventDefault();
+  $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
 });
+
+
+$(document).on('scroll', function () {
+    if ( $(this).scrollTop() > 220 ) {
+      $('.description__title h2').addClass('animated slideInLeft');
+      $('.description__skills h2').addClass('animated slideInLeft');
+    }
+    if ( $(this).scrollTop() > 1200 ) {
+      $('.portafolio__item').addClass('animated zoomIn');
+    }
+  });
