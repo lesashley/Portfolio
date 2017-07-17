29da6c6f66d21dcf838c1fb3f09f9cf8bc9baed8
lesahley <lesashley56@gmail.com>
2017-07-17 00:59:25 -0500

    Agregando modal y textos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
- $('a').click(function(e){
+ $('.menu').click(function(e){
  	e.preventDefault();
  	$('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
  });
+
+(_ =>{
+  // $('#description').scroll(function () {
+    // var n = $('#description').scrollTop();
+    // console.log(n);
+    // console.log("hola");
+  // });
+console.log($('#description').scrollTop());
+});
