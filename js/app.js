740cbba04bd7cb9f0217ccf88d55bfe94b9795cb
lesahley <lesashley56@gmail.com>
2017-07-18 15:07:18 -0500

    Cambio de tipografia

--- js/app.js
+++ js/app.js
@@ -13,3 +13,11 @@ $(document).on('scroll', function () {
       $('.portafolio__item').addClass('animated zoomIn');
     }
   });
+
+
+// $('.preload').show(9000);
+// $('.body-container').hide();
+// $(document).ready(function(){
+//   $('.preload').hide(1000);
+//   $('.body-container').show(2000)
+//   });
--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -4,14 +4,9 @@ window.addEventListener("scroll", function () {
   var header = document.getElementById("header");
   if (currentScroll > lastScrollTop) {
     header.classList.add("header-hidden");
-    header.style.opacity = 1;
-  } else{
-    header.style.opacity = 0;
+  }
+  else{
     header.classList.remove("header-hidden");
-    if (currentScroll <=3) {
-      header.classList.remove("header-hidden");
-      header.style.opacity = 1;
     }
   }
-  lastScrollTop = currentScroll;
-},false);
+);
