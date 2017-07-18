d86fc19d4838f76c68002331e208d664620b977a
lesahley <lesashley56@gmail.com>
2017-07-18 07:16:57 -0500

    cambio del tamaño de margenes

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -3,13 +3,14 @@ window.addEventListener("scroll", function () {
   var currentScroll = window.pageYOffset || document.body.scrollTop;
   var header = document.getElementById("header");
   if (currentScroll > lastScrollTop) {
-    header.classList.remove("header-hidden");
-    header.style.opacity = 0;
-  } else{
     header.classList.add("header-hidden");
     header.style.opacity = 1;
-    if (currentScroll <=3) {
+  } else{
+    header.style.opacity = 0;
     header.classList.remove("header-hidden");
+    if (currentScroll <=3) {
+      header.classList.remove("header-hidden");
+      header.style.opacity = 1;
     }
   }
   lastScrollTop = currentScroll;
